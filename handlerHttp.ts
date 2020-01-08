fd28a38c3f246a2d2c4d00135d3dfc147a89fe96
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2020-01-08 12:48:10 +0000

    Add comments to the API Gateway handler

--- handlerHttp.ts
+++ handlerHttp.ts
@@ -3,9 +3,17 @@ import { apiGatewayAdapter } from "./app/sources/apiGatewayAdapter";
 import { StubAmazingEnergyClient } from "./app/accountClients/StubAmazingEnergyClient";
 import { StubInstrumentation } from "./app/instrumentation/StubInstrumentation";
 
+// Create the dependencies for the core functionality
 const closeAccountDeps = {
+  // Implements Instrumentation interface (port)
   instrumentation: new StubInstrumentation(),
+
+  // Implements AccountManager interface (port)
   accountManager: new StubAmazingEnergyClient(),
 };
 
-export const handler = apiGatewayAdapter(closeAccount(closeAccountDeps));
+// Initialise the handler with the apiGatewayAdaptor which depends on the CloseAccount port
+export const handler = apiGatewayAdapter(
+  // Creates function that implements CloseAccount (port)
+  closeAccount(closeAccountDeps)
+);
