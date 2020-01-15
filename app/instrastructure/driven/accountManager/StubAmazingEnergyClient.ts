c919f4116c5a54ed9c86395e960b8387171d7fce
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2020-01-15 17:26:02 +0000

    Fix import

--- app/instrastructure/driven/accountManager/StubAmazingEnergyClient.ts
+++ app/instrastructure/driven/accountManager/StubAmazingEnergyClient.ts
@@ -1,5 +1,5 @@
-import { AccountManager } from "./";
 import { FuelType, Meter, Unit } from "../../../domain/models/Meter";
+import { AccountManager } from "./AccountManager";
 
 export class StubAmazingEnergyClient implements AccountManager {
   private static readonly ELECTRICITY_METER: Readonly<Meter> = {
